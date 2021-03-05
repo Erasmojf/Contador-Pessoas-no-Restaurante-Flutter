ff9acd7f0b4eea45e2a4fedd224b23312c5c7ac2
Wings Entregas <wingsentregas@gmail.com>
2021-03-05 10:46:42 -0300

    Commit first created count people in restaurant
    Commit first created count people in restaurant

---
+++ contador_pessoas_restaurante/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:contador_pessoas_restaurante/ui/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'Contador de pessoas no restaurante',
+    home: HomePage(),
+  ));
+}
